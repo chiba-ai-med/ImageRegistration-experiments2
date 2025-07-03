fd8b4621f28de57d58ab5fd629a0c977673c905d
kokitsuyuzaki <k.t.the-answer@hotmail.co.jp>
2025-07-03 10:52:32 +0900

    First Commit

--- workflow/dag.sh
+++ workflow/dag.sh
@@ -1,6 +1,5 @@
 # DAG graph
 snakemake -s workflow/preprocess.smk --rulegraph | dot -Tpng > plot/preprocess.png
-snakemake -s workflow/ir.smk --rulegraph | dot -Tpng > plot/ir.png
 snakemake -s workflow/ot.smk --rulegraph | dot -Tpng > plot/ot.png
 snakemake -s workflow/guidedpls.smk --rulegraph | dot -Tpng > plot/guidedpls.png
 snakemake -s workflow/evaluation.smk --rulegraph | dot -Tpng > plot/evaluation.png
